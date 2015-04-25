d6682974cfe1ce5b144c15ed9eaf553082a5b45c
Alex Kavourias <kavourias.a@Husky.neu.edu>
2015-04-25 13:26:47 -0400

    skeleton for slack client

--- slack.py
+++ slack.py
@@ -1,7 +1,35 @@
 from slackclient import SlackClient
+import time
+import os
 
-def get_client(token):
-    return SlackClient(token)
 
-token = open('token.txt').read()
-print get_client(token)
+class SlackLine(object):
+
+    def __init__(self, token=None, channel_id=None, channel_name=None):
+        assert token or 'token.txt' in os.listdir(os.getcwd())
+        self.client = self.get_client(token if token else open('token.txt').read())
+        self.server = self.client.server
+        self._logs = []
+        #Attach All Channels in the User's view
+        #TODO
+
+    def switch_channel(self): #TODO
+        pass
+
+    def get_client(self, token):
+        return SlackClient(token)
+
+    def chat_forever(self):
+        print 'Welcome to SlackLine!'
+        print '\t Talking in Chat : %s' % self.current_channel
+        while True:
+            cmd = raw_input('>>> ')
+            if cmd.startswith('send'):
+                #send message
+                pass
+            if cmd.startswith('quit'):
+                self.flush_logs()
+                break
+        cmd = raw_input('Exit SlackLine? [Y/n]')
+
+client = SlackLine().client
